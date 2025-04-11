009440a758560bb1305b73dabc325c599abfb1c7
Peter Kofler <peter.kofler@code-cop.org>
2025-04-11 12:04:12 +0000

    Decompile.

--- Simple Encryptor/rev_simpleencryptor/encrypt_ghidra.c
+++ Simple Encryptor/rev_simpleencryptor/encrypt_ghidra.c
@@ -1,42 +1,3 @@
-# 4. Simple Encryptor
-
-password: hackthebox
-
-Das `flag.enc` ist ein binary file, 33 bytes lang.
-
-`encrypt`ist ein ELF file, normal.
-
-## strings
-
-´´´
-u+UH
-dH3<%(
-[]A\A]A^A_
-flag
-flag.enc
-:*3$"
-´´´
-
-## Execute
-
-rm flag.enc 
-mv something.txt flag
-$ ./encrypt
-
-* neues flag war 12 Characters
-* flag.enc ist 17 Characters
-* Wir sehen kein Pattern
-
-## ltrace/strace
-
-Es lädt die Datei, verschlüsselt mit random und schreibt dann das enc file.
-
-## asm
-
-## Ghidra
-
-https://en.cppreference.com/w/
-
 undefined8 main(void)
 
 {
@@ -82,5 +43,3 @@ undefined8 main(void)
   }
   return 0;
 }
-
-###
