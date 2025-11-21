fd9986bbb82f4c1c17dfdc5ab00d14fcecc9fdc9
Peter Kofler <peter.kofler@code-cop.org>
2025-11-21 13:32:08 +0100

    Copy project to rename fields and methods.

---
+++ Bypass/rev_bypass/decompiled/4.cs
@@ -0,0 +1,12 @@
+using System;
+
+[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class)]
+public class _4 : Attribute
+{
+	public string _0;
+
+	public _4()
+	{
+		_0 = _5.c;
+	}
+}
---
+++ Bypass/rev_bypass/decompiled/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyDescription("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("HTBChallange")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: Guid("5089226f-12b6-4050-9ce3-f7c43721a5a2")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle("HTBChallange")]
+[assembly: AssemblyVersion("1.0.0.0")]
